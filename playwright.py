8b81fe95170f0e1d0eb911ea79daa4edbd5f664d
Prateekc13 <130299516+Prateek13052003@users.noreply.github.com>
2026-05-02 17:31:19 +0530

    Add files via upload

---
+++ playwright.py
@@ -0,0 +1,116 @@
+# %%
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from IPython.display import Image, display
+import gradio as gr
+from langgraph.prebuilt import ToolNode, tools_condition
+import requests
+import os
+from langchain.agents import Tool
+from langgraph.checkpoint.memory import MemorySaver
+
+# %%
+load_dotenv(override=True)
+
+# %%
+class State(TypedDict):
+    
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+# %%
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_url = "https://api.pushover.net/1/messages.json"
+
+def push(text: str):
+    """Send a push notification to the user"""
+    requests.post(pushover_url, data = {"token": pushover_token, "user": pushover_user, "message": text})
+
+tool_push = Tool(
+        name="send_push_notification",
+        func=push,
+        description="useful for when you want to send a push notification"
+    )
+
+# %%
+
+
+import nest_asyncio
+nest_asyncio.apply()
+
+# %%
+from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+from langchain_community.tools.playwright.utils import create_async_playwright_browser
+
+async_browser = create_async_playwright_browser(headless=False)
+
+toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
+tools = toolkit.get_tools()
+
+# %%
+for tool in tools:
+    print(f"{tool.name}={tool}")
+
+# %%
+tool_dict = {tool.name:tool for tool in tools}
+
+navigate_tool = tool_dict.get("navigate_browser")
+extract_text_tool = tool_dict.get("extract_text")
+
+    
+await navigate_tool.arun({"url": "https://www.cnn.com"})
+text = await extract_text_tool.arun({})
+
+# %%
+import textwrap
+print(textwrap.fill(text))
+
+# %%
+all_tools = tools + [tool_push]
+
+# %%
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model="llama3.1",
+    base_url="http://localhost:11434"
+)
+
+llm_with_tools = llm.bind_tools(all_tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# %%
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=all_tools))
+graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+# %%
+config = {"configurable": {"thread_id": "10"}}
+
+async def chat(user_input: str, history):
+    result = await graph.ainvoke({"messages": [{"role": "user", "content": user_input}]}, config=config)
+    return result["messages"][-1].content
+
+
+gr.ChatInterface(chat, type="messages").launch()
+
+# %%
+
+
+
